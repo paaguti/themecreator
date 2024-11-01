5a801a50f2d0bde5958f6dc483f76d96e15bce1d
Pedro A. Aranda <paaguti@gmail.com>
2024-11-01 07:54:10 +0100

    Include basic cleanup

--- build-run.sh
+++ build-run.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-cd $(dirname $0)
+function cleanup () {
+  [ -n "$(docker ps -aq -f status=running)" ] && \
+	  docker container kill test
+  ZOMBIES="$(docker ps -aq -f status=exited)"
+  [ -n "$ZOMBIES" ] && docker rm "$ZOMBIES"
+}
 
-docker build -t themecreator:test .
+trap "cleanup" EXIT
+
+cd $(dirname $0)
+if [ "$(docker image ls themecreator | wc -l)" == "2" ]; then
+  echo "Building new image"
+  docker build -t themecreator:test .
+fi
 docker run --network=host -it \
        -u $UID:$GID -v $(pwd)/app.core:/usr/src/app \
        --name test themecreator:test
